ea966a25707b9a17ee106e4a06fdd552a8bddca1
Johnny Gérard <john@jgerard.dev>
2025-01-14 15:11:45 +0100

    refactor: rename "state" to "game"

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -11,7 +11,7 @@ import { memo } from "react";
 import { useImmerReducer } from "use-immer";
 
 const Game = () => {
-  const [state, dispatch] = useImmerReducer(
+  const [game, dispatch] = useImmerReducer(
     gameReducer,
     new GameState(new BoardState()),
   );
@@ -19,22 +19,22 @@ const Game = () => {
   return (
     <main className="flex flex-col items-center gap-8">
       <div className="flex w-full justify-between">
-        <Stopwatch gameStatus={state.status} />
-        <MineCounter remaining={state.board.mines - state.board.flags} />
+        <Stopwatch gameStatus={game.status} />
+        <MineCounter remaining={game.board.mines - game.board.flags} />
       </div>
-      <IsGameOverContext value={state.isGameOver}>
+      <IsGameOverContext value={game.isGameOver}>
         <DispatchContext.Provider value={dispatch}>
-          <Board cells={state.board.cells} columns={state.board.columns} />
+          <Board cells={game.board.cells} columns={game.board.columns} />
         </DispatchContext.Provider>
       </IsGameOverContext>
-      {state.status !== "INITIAL" && (
+      {game.status !== "INITIAL" && (
         <button type="button" onClick={() => dispatch({ type: "RESTART" })}>
           {
             {
               PLAYING: "Restart",
               WON: "Play again",
               LOST: "Try again",
-            }[state.status]
+            }[game.status]
           }
         </button>
       )}
