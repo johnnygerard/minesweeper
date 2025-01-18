1df4141b4d34782bd352ff702474d96532794f19
Johnny Gérard <john@jgerard.dev>
2025-01-18 16:11:43 +0100

    feat: add icon link to game modes

--- src/components/game-page.tsx
+++ src/components/game-page.tsx
@@ -1,5 +1,7 @@
 import Game from "@/components/game";
 import { GameMode } from "@/types/game-mode";
+import { CompassRose } from "@phosphor-icons/react/dist/ssr";
+import Link from "next/link";
 import { memo } from "react";
 
 type Props = Readonly<{
@@ -9,7 +11,17 @@ type Props = Readonly<{
 const GamePage = ({ mode }: Props) => {
   return (
     <div className="px-4 py-8">
-      <h1 className="mb-12 text-4xl uppercase tracking-widest">Minesweeper</h1>
+      <div className="mb-12 flex justify-between text-4xl">
+        <h1 className="uppercase tracking-widest">Minesweeper</h1>
+        <Link
+          className="transition-transform hover:scale-110 hover:text-blue-500"
+          href="/game-modes"
+          title="Game Modes"
+          aria-label="Game Modes"
+        >
+          <CompassRose />
+        </Link>
+      </div>
       <Game mode={mode} />
     </div>
   );
