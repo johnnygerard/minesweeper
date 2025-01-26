e9e844ccbe8b736ec03f3d2713930edbf3ca7cbf
Johnny Gérard <john@jgerard.dev>
2025-01-26 20:10:24 +0100

    refactor: introduce navbar.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
-import { BookBookmark, CompassRose } from "@phosphor-icons/react/dist/ssr";
 import "./globals.css";
+import Navbar from "@/components/navbar";
 import clsx from "clsx";
 import type { Metadata } from "next";
 import { Aldrich } from "next/font/google";
-import Link from "next/link";
 import { memo, ReactNode } from "react";
 
 const aldrich = Aldrich({
@@ -51,34 +50,7 @@ const RootLayout = ({ children }: Props) => {
       lang="en-US"
     >
       <body className="flex min-h-screen min-w-min flex-col bg-zinc-50">
-        <nav className="border-b border-zinc-200 bg-white">
-          <div className="mx-auto flex max-w-7xl justify-between p-4">
-            <Link
-              href="/"
-              className="text-2xl uppercase tracking-widest transition-colors hover:text-blue-500"
-            >
-              Minesweeper
-            </Link>
-            <div className="flex gap-4">
-              <Link
-                className="transition-colors transition-transform hover:scale-110 hover:text-blue-500"
-                href="/game-modes"
-                title="Game Modes"
-                aria-label="Game Modes"
-              >
-                <CompassRose size="2rem" />
-              </Link>
-              <Link
-                className="transition-colors transition-transform hover:scale-110 hover:text-blue-500"
-                href="/how-to-play"
-                title="How to Play"
-                aria-label="How to Play"
-              >
-                <BookBookmark size="2rem" />
-              </Link>
-            </div>
-          </div>
-        </nav>
+        <Navbar />
         <main className="grid flex-1 place-items-center px-4 py-8">
           {children}
         </main>
---
+++ src/components/navbar.tsx
@@ -0,0 +1,38 @@
+import { BookBookmark, CompassRose } from "@phosphor-icons/react/dist/ssr";
+import Link from "next/link";
+import { memo } from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="border-b border-zinc-200 bg-white">
+      <div className="mx-auto flex max-w-7xl justify-between p-4">
+        <Link
+          href="/"
+          className="text-2xl uppercase tracking-widest transition-colors hover:text-blue-500"
+        >
+          Minesweeper
+        </Link>
+        <div className="flex gap-4">
+          <Link
+            className="transition-colors transition-transform hover:scale-110 hover:text-blue-500"
+            href="/game-modes"
+            title="Game Modes"
+            aria-label="Game Modes"
+          >
+            <CompassRose size="2rem" />
+          </Link>
+          <Link
+            className="transition-colors transition-transform hover:scale-110 hover:text-blue-500"
+            href="/how-to-play"
+            title="How to Play"
+            aria-label="How to Play"
+          >
+            <BookBookmark size="2rem" />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default memo(Navbar);
